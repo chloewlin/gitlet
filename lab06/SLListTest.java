d4078042b123a02ea4cbdbc3a2cc4a1264e2d0ee
Matthew Owen <omatthew98@berkeley.edu>
2020-06-29 17:12:05 -0700

    adding SLListTest

--- lab06/SLListTest.java
+++ lab06/SLListTest.java
@@ -1,128 +1,26 @@
-/**
- * An SLList is a list of integers, which encapsulates the
- * naked linked list structure.
- */
-public class SLList {
+import org.junit.Test;
+import static org.junit.Assert.*;
 
-    /**
-     * IntListNode is a nested class that represents a single node in the
-     * SLList, storing an item and a reference to the next IntListNode.
-     */
-    private static class IntListNode {
-        /*
-         * The access modifiers inside a private nested class are irrelevant:
-         * both the inner class and the outer class can access these instance
-         * variables and methods.
-         */
-        public int item;
-        public IntListNode next;
+public class SLListTest {
 
-        public IntListNode(int item, IntListNode next) {
-            this.item = item;
-            this.next = next;
-        }
+    @Test
+    public void testSLListAdd() {
+        SLList test1 = SLList.of(1, 3, 5);
+        SLList test2 = new SLList();
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            IntListNode that = (IntListNode) o;
-            return item == that.item;
-        }
+        test1.add(1, 2);
+        test1.add(3, 4);
+        assertEquals(5, test1.size());
+        assertEquals(3, test1.get(2));
+        assertEquals(4, test1.get(3));
 
-        @Override
-        public String toString() {
-            return item + "";
-        }
+        test2.add(1, 1);
+        assertEquals(1, test2.get(0));
+        assertEquals(1, test2.size());
 
-    }
-
-    /* The first item (if it exists) is at sentinel.next. */
-    private IntListNode sentinel;
-    private int size;
-
-    /** Creates an empty SLList. */
-    public SLList() {
-        sentinel = new IntListNode(42, null);
-        sentinel.next = sentinel;
-        size = 0;
-    }
-
-    public SLList(int x) {
-        sentinel = new IntListNode(42, null);
-        sentinel.next = new IntListNode(x, null);
-        sentinel.next.next = sentinel;
-        size = 1;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        SLList slList = (SLList) o;
-        if (size != slList.size) return false;
-
-        IntListNode l1 = sentinel.next;
-        IntListNode l2 = slList.sentinel.next;
-
-        while (l1 != sentinel && l2 != slList.sentinel) {
-            if (!l1.equals(l2)) return false;
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        IntListNode l = sentinel.next;
-        String result = "";
-
-        while (l != sentinel) {
-            result += l + " ";
-            l = l.next;
-        }
-
-        return result.trim();
-    }
-
-    /** Returns an SLList consisting of the given values. */
-    public static SLList of(int... values) {
-        SLList list = new SLList();
-        for (int i = values.length - 1; i >= 0; i -= 1) {
-            list.addFirst(values[i]);
-        }
-        return list;
-    }
-
-    /** Returns the size of the list. */
-    public int size() {
-        return size;
-    }
-
-    /** Adds x to the front of the list. */
-    public void addFirst(int x) {
-        sentinel.next = new IntListNode(x, sentinel.next);
-        size += 1;
-    }
-
-    /** Return the value at the given index. */
-    public int get(int index) {
-        IntListNode p = sentinel.next;
-        while (index > 0) {
-            p = p.next;
-            index -= 1;
-        }
-        return p.item;
-    }
-
-    /** Adds x to the list at the specified index. */
-    public void add(int index, int x) {
-        // TODO
-    }
-
-    /** Destructively reverses this list. */
-    public void reverse() {
-        // TODO
+        test2.add(10, 10);
+        assertEquals(10, test2.get(1));
+        test1.add(0, 0);
+        assertEquals(SLList.of(0, 1, 2, 3, 4, 5), test1);
     }
 }
