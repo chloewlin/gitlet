d2ddb4873d468bee54c6a1a1ab508d75b2681191
Chloe Lin <chloewjlin@gmail.com>
2020-07-02 23:01:36 -0700

    Add basic tests for addFirst

--- proj1/ArrayDequeTest.java
+++ proj1/ArrayDequeTest.java
@@ -5,8 +5,19 @@ public class ArrayDequeTest {
 
     @Test
     public void arrayLengthTest() {
-
         ArrayDeque<String> strArray = new ArrayDeque<>();
         assertEquals(8, strArray.size());
+    }
+
+    @Test
+    public void addFirstTest() {
+        ArrayDeque<String> strArray = new ArrayDeque<>();
+        assertEquals(8, strArray.size());
+        strArray.addFirst("zero");
+        strArray.printDeque();
+        strArray.addFirst("one");
+        strArray.printDeque();
+        strArray.addFirst("two");
+        strArray.printDeque();
     }
 }
