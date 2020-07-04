dde312cbfaf1a64f3b67a4f92b13ca056bc59778
Christal Huang <shihuang@berkeley.edu>
2020-07-03 21:47:39 -0700

    Updates add&remove function, and get function

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -6,6 +6,15 @@ public class ArrayDeque<T> implements Deque<T> {
     private int nextBack;
     private T[] array;
 
+    public int addOne(int i) {
+        return (i + 1) % this.array.length;
+    }
+
+    public int minusOne(int i) {
+        return (i - 1 + this.array.length) % this.array.length;
+    }
+
+
     /**
      *Creates an empty array deque, again the starting size should be 8
      */
@@ -34,12 +43,9 @@ public class ArrayDeque<T> implements Deque<T> {
         if (this.size == this.array.length) {
             resize(this.array.length * 2);
         }
-
-        if (this.size < this.array.length) {
-            this.array[this.nextFront] = item;
-            this.size++;
-            this.nextFront = (this.nextFront - 1 + this.array.length) % this.array.length;
-        }
+        this.array[this.nextFront] = item;
+        this.size++;
+        this.nextFront = minusOne(this.nextFront); //minusOne
     }
 
     /**
@@ -49,12 +55,9 @@ public class ArrayDeque<T> implements Deque<T> {
         if (this.size == this.array.length) {
             resize(this.array.length * 2);
         }
-
-        if (this.size < this.array.length) {
-            this.array[this.nextBack] = item;
-            this.size++;
-            this.nextBack = (this.nextBack + 1) % this.array.length;
-        }
+        this.array[this.nextBack] = item;
+        this.size++;
+        this.nextBack = addOne(this.nextBack); //addOne
     }
 
     /**
@@ -89,38 +92,46 @@ public class ArrayDeque<T> implements Deque<T> {
      * Shrinks array
      */
     private void shrink() {
-        T[] a = (T[]) new Object[this.array.length / 2];
+        T[] a = (T[]) new Object[this.array.length/2];
         System.arraycopy(this.array, this.nextFront + 1, a,0, this.size);
         this.array = a;
         this.nextFront = this.array.length - 1;
         this.nextBack = this.size;
-//        System.out.println(this.nextFront);
-//        System.out.println(this.nextBack);
+        System.out.println(this.nextFront);
+        System.out.println(this.nextBack);
     }
     /**
      * Removes and returns the item at the nextFront of the deque.
      * If no such item exists, returns null
      */
     public T removeFirst() {
-        T frontValue = null;
-        if (this.isEmpty()) {
+        if (isEmpty()) {
             return null;
-        }
-        if (this.array.length >= 16 && this.size <= this.array.length * 0.25) {
+       } else {
+            int newFront = addOne(this.nextFront);
+            T frontValue = this.array[newFront];
+            this.array[newFront] = null;
+            size--;
+            this.nextFront = newFront;
+            if (this.array.length > 8 && this.size <= this.array.length * 0.25) {
+                shrink();
+            }
+            return frontValue;
+
+//        if (this.nextFront == this.array.length - 1) {
+//           frontValue = this.array[0];
+//           this.array[0] = null;
+//           this.nextFront = 0;
+//        } else {
+//            frontValue = this.array[this.nextFront + 1];
+//            this.array[this.nextFront + 1] = null;
+//            this.nextFront = addOne(this.nextFront); //addOne
+//        }
+//            this.size--;
+//        if (this.array.length > 8 && this.size <= this.array.length * 0.25) {
 //            resize(this.array.length / 2);
-            shrink();
-        }
-        if (this.nextFront == this.array.length - 1) {
-            frontValue = this.array[0];
-            this.array[0] = null;
-            this.nextFront = 0;
-        } else {
-            frontValue = this.array[this.nextFront + 1];
-            this.array[this.nextFront + 1] = null;
-            this.nextFront = (this.nextFront + 1) % this.array.length;
-        }
-        this.size--;
-        return frontValue;
+//            shrink();
+       }
     }
 
     /**
@@ -128,25 +139,34 @@ public class ArrayDeque<T> implements Deque<T> {
      * If no such item exists, returns null
      */
     public T removeLast() {
-        T backValue = null;
         if (this.isEmpty()) {
             return null;
-        }
-        if (this.array.length >= 16 && this.size < (this.array.length/4)) {
-            resize(this.array.length / 2);
-        }
-        if (this.nextBack == 0) {
-            backValue = this.array[this.array.length - 1];
-            this.array[this.array.length - 1] = null;
-            this.nextBack = 0;
         } else {
-            backValue = this.array[this.nextBack - 1];
-            this.array[this.nextBack - 1] = null;
-            this.nextBack = (this.nextBack - 1) % this.array.length;
+            int newBack = minusOne(this.nextBack);
+            T backValue = this.array[newBack];
+            this.array[newBack] = null;
+            size--;
+            this.nextBack = newBack;
+            if (this.array.length > 8 && this.size <= this.array.length * 0.25) {
+                shrink();
+            }
+            return backValue;
         }
-        this.size--;
-
-        return backValue;
+//        if (this.array.length >= 16 && this.size < (this.array.length/4)) {
+//            resize(this.array.length / 2);
+//        }
+//        if (this.nextBack == 0) {
+//            backValue = this.array[this.array.length - 1];
+//            this.array[this.array.length - 1] = null;
+//            this.nextBack = 0;
+//        } else {
+//            backValue = this.array[this.nextBack - 1];
+//            this.array[this.nextBack - 1] = null;
+//            this.nextBack = (this.nextBack - 1) % this.array.length;
+//        }
+//        this.size--;
+//
+//        return backValue;
     }
 
     /**
@@ -155,9 +175,11 @@ public class ArrayDeque<T> implements Deque<T> {
      * returns null. Must not alter the deque
      */
     public T get(int index) {
-        if (index > this.array.length - 1 || index < 0) {
+        if (index >= this.size || index < 0) {
             return null;
         }
-        return this.array[index];
+        int frontIndex = addOne(this.nextFront);
+        int actualIndex = (frontIndex + index) % this.array.length;g
+        return this.array[actualIndex];
     }
 }
