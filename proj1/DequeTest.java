c0bed11334df5f3c7d2c84e7fa062dac1d5543b5
Chloe Lin <chloewjlin@gmail.com>
2020-07-04 23:53:01 -0700

    Add random tests for Deque interface
    - Use random to create a large number of insertions, then deletions
    down to size 0, then insertion. ArrayDeque and LinkedListDeque
    should behave the same: they should have the same size and same
    order.

--- proj1/DequeTest.java
+++ proj1/DequeTest.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.*;
 
 public class DequeTest {
+
+    private int generateRandom(int low, int high) {
+        Random g = new Random();
+        return g.nextInt(high - low) + low;
+    };
+
+    @Test
+    public void DequeTest() {
+        Deque<Object> arr = new ArrayDeque<>();
+        Deque<Object> lld = new LinkedListDeque<>();
+
+        int r1 = generateRandom(600, 1000);
+        for (int i = 0; i < r1; i++) {
+            arr.addFirst(i);
+            lld.addFirst(i);
+        }
+        assertEquals("arr should have the correct size", r1, arr.size());
+        assertEquals("arr should return the first item", r1 - 1, arr.get(0));
+        assertEquals("list should have the correct size", r1, lld.size());
+        assertEquals("list should return the first item", r1 - 1, lld.get(0));
+
+        for (int i = 0; i < r1; i++) {
+            arr.removeFirst();
+            lld.removeFirst();
+        }
+        assertTrue("arr should shrink when it has 0 items", arr.isEmpty());
+        assertNull("arr should return null when it is empty", arr.get(0));
+        assertTrue("list should be empty", lld.isEmpty());
+        assertNull("list should return null when it is empty", lld.get(0));
+
+        int r2 = generateRandom(1000, 2000);
+        for (int i = 0; i < r2; i++) {
+            arr.addLast(i);
+            lld.addLast(i);
+        }
+        assertEquals("arr should resize when it has a large number of insertions", r2, arr.size());
+        assertEquals("arr should return the first item", 0, arr.get(0));
+        assertEquals("list should hold a large number of insertions", r2, lld.size());
+        assertEquals("list should return the first item", 0, lld.get(0));
+    }
 }
