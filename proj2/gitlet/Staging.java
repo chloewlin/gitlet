e480725ac42e8325f81d516821610e94192a539c
Chloe Lin <chloewjlin@gmail.com>
2020-07-14 21:36:30 -0700

    Create a Staging class to hold blobs added to staging

--- proj2/gitlet/Staging.java
+++ proj2/gitlet/Staging.java
@@ -1,4 +1,15 @@
 package gitlet;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Staging {
+    private Map staged;
+
+    public Staging() {
+        this.staged = new HashMap<>();
+    }
+
+    public void add(Blob blob) {
+    }
 }
