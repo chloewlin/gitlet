2d23cf584b15f2f006039692ff9c86ea2af7fb5b
Chloe Lin <chloewjlin@gmail.com>
2020-07-14 23:52:16 -0700

    Comment out unused print logs

--- proj2/gitlet/Staging.java
+++ proj2/gitlet/Staging.java
@@ -14,8 +14,8 @@ public class Staging {
 
     public void add(Blob blob) {
         this.trackedFiles.put(blob.fileName, blob.fileContent);
-        System.out.println("Blob " + blob.fileName);
-        System.out.println("BlobContent " + blob.fileContent);
-        System.out.println("Blob SHA" + blob.fileSHA1);
+//        System.out.println("Blob " + blob.fileName);
+//        System.out.println("BlobContent " + blob.fileContent);
+//        System.out.println("Blob SHA" + blob.fileSHA1);
     }
 }
