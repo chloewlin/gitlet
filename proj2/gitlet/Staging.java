681b16e0058d0830ededc1daf1613c72295e3a5f
Chloe Lin <chloewjlin@gmail.com>
2020-07-14 23:49:25 -0700

    Create hashmap in staging class that keeps track of file-blob mapping

--- proj2/gitlet/Staging.java
+++ proj2/gitlet/Staging.java
@@ -4,12 +4,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Staging {
-    private Map staged;
+    Map<String, byte[]> trackedFiles;
+    Map<String, byte[]> untrackedFiles;
 
     public Staging() {
-        this.staged = new HashMap<>();
+        this.trackedFiles = new HashMap<String, byte[]>();
+        this.untrackedFiles = new HashMap<String, byte[]>();
     }
 
     public void add(Blob blob) {
+        this.trackedFiles.put(blob.fileName, blob.fileContent);
+        System.out.println("Blob " + blob.fileName);
+        System.out.println("BlobContent " + blob.fileContent);
+        System.out.println("Blob SHA" + blob.fileSHA1);
     }
 }
