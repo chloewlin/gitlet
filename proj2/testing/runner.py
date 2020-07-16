f7d3ed951daa5d182bcfc7c0d41986b715b1eabd
Matthew Owen <omatthew98@berkeley.edu>
2020-07-15 22:30:23 -0700

    updating runner.py to work with paths that have spaces

--- proj2/testing/runner.py
+++ proj2/testing/runner.py
@@ -471,7 +471,7 @@ if __name__ == "__main__":
         JAVA_COMMAND = 'exec ' + JAVA_COMMAND
         JAVAC_COMMAND = 'exec ' + JAVAC_COMMAND
 
-    compile_target = join(gitlet_dir, "*.java")
+    compile_target = join('"' + gitlet_dir + '"', "*.java")
     msg, output = doCompile(compile_target)
     if output.find("error") >= 0:
         print(output)
