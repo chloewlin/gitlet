b6b3ec82c4d5c141989ca09e53f539373baf7c4b
Chloe Lin <chloewjlin@gmail.com>
2020-07-16 03:49:51 -0700

    Organize instance var of Commit obj and add Hashmap
    - add Hashmap to store mapping between the files that it is tracking
    and the blobs
    - To-do: complete the functionality to track files in a commit using
    hashing

--- proj2/gitlet/Commit.java
+++ proj2/gitlet/Commit.java
@@ -14,20 +14,12 @@ import java.util.TimeZone;
 
 public class Commit implements Serializable {
 
-    /* arrays for SHA1 parents commits, start with all 0s */
     private String[] parentsArray = new String[2];
-
-    /* SHA1 for current commit */
     public String SHA;
-
-    /* timestamp when the commit is generated */
     public String timestamp;
-
-    /* commit message */
     public String message;
-
     /* mapping between file SHA1 and blob SHA1 */
-    private HashMap map;
+    private HashMap<String, String> map;
 
     private boolean init = false;
 
@@ -39,6 +31,7 @@ public class Commit implements Serializable {
         this.parentsArray[0] = parent;
         this.SHA = Utils.sha1(message);
         this.timestamp = generateDate(init);
+        this.map = new HashMap<>();
     }
 
     public void saveCommit() throws IOException {
