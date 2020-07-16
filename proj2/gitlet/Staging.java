7bb610ab75bdc09cfedb0efdf8ccb6fd4a7a08b3
Chloe Lin <chloewjlin@gmail.com>
2020-07-16 02:21:10 -0700

    Write the functionality to store mapping of file and blobSHA in staging
    - When we stage a file, we want to temporarily save it in a staging
    area (aka a staging obj). To persist our data, we want to save
    the file/blobSHA mapping into a hashmap and write it into a file
    - format: Hello.txt:$1d229271928d3f9e2bb0375bd6ce5db6c6d348d9
    - add helper methods: save() will write the obj, print() will print
    the key-value pairs in our hashmap

--- proj2/gitlet/Staging.java
+++ proj2/gitlet/Staging.java
@@ -1,18 +1,36 @@
 package gitlet;
 
+import java.io.File;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Staging {
-    Map<String, byte[]> trackedFiles;
-    Map<String, byte[]> untrackedFiles;
+public class Staging implements Serializable {
+
+    static final File STAGING_FOLDER = Utils.join(".gitlet", "staging");
+    Map<String, String> trackedFiles;
+    Map<String, String> untrackedFiles;
 
     public Staging() {
-        this.trackedFiles = new HashMap<String, byte[]>();
-        this.untrackedFiles = new HashMap<String, byte[]>();
+        this.trackedFiles = new HashMap<String, String>();
+        this.untrackedFiles = new HashMap<String, String>();
     }
 
-    public void add(Blob blob) {
-        this.trackedFiles.put(blob.fileName, blob.fileContent);
+    /**
+     * Store the mapping of file and blob like so:
+     * Hello.txt:$1d229271928d3f9e2bb0375bd6ce5db6c6d348d9
+     * */
+    public void add(String fileName, String blobSHA1) {
+        this.trackedFiles.put(fileName, blobSHA1);
+    }
+
+    public void save(Staging stagedFiles) {
+        File currentTrackedFiles = Utils.join(STAGING_FOLDER, "trackedFiles");
+        Utils.writeObject(currentTrackedFiles, stagedFiles);
+    }
+
+    public void print() {
+        trackedFiles
+                .forEach((key, value) -> System.out.println(key + ":" + value));
     }
 }
