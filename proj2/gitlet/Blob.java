65a4bd95460df01a76d9e0d627d6578333aaec50
Chloe Lin <chloewjlin@gmail.com>
2020-07-17 02:08:26 -0700

    Run style check and clean up style in Blob.java

--- proj2/gitlet/Blob.java
+++ proj2/gitlet/Blob.java
@@ -3,27 +3,47 @@ package gitlet;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * A Blob class saves a staged file as a byte array.
+ *
+ * @author Chloe Lin, Christal Huang
+ */
 public class Blob {
 
+    /**
+     * The name of a staged file.
+     */
     private String fileName;
+    /**
+     * The content of a staged file.
+     */
     private byte[] fileContent;
+    /**
+     * The SHA1 of a blob object.
+     */
     private String blobSHA1;
-    static final File BLOBS_FOLDER = new File(".gitlet/objects/blobs");
-
-    /* Create a blob for a file */
-    public Blob(String fileName) {
-        this.fileName = fileName;
-        this.fileContent = Utils.readContents(Utils.join(".", fileName));
+    /**
+     * Create a blob for a file.
+     * @param filename the name of the staged file.
+     */
+    public Blob(String  filename) {
+        this.fileName = filename;
+        this.fileContent = Utils.readContents(Utils.join(".", filename));
         this.blobSHA1 = Utils.sha1("BLOB", fileContent);
     }
 
-    /* Save blob as a file */
+    /**
+     * Save a blob as a byte array.
+     */
     public void save() throws IOException {
-        File blobFile =  Utils.join(BLOBS_FOLDER, blobSHA1);
+        File blobFile =  Utils.join(Main.BLOBS_FOLDER, blobSHA1);
         blobFile.createNewFile();
         Utils.writeObject(blobFile, fileContent);
     }
 
+    /**
+     * Return SHA1 of a blob.
+     */
     public String getBlobSHA1() {
         return this.blobSHA1;
     }
