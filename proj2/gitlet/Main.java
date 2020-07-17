96e484b4dd47580acc01fb1fdd7df084b18da054
Chloe Lin <chloewjlin@gmail.com>
2020-07-17 15:57:37 -0700

    Add break to break from the operand switch statement

--- proj2/gitlet/Main.java
+++ proj2/gitlet/Main.java
@@ -88,6 +88,7 @@ public class Main {
              break;
         case "checkout":
              validateCheckout(args);
+             break;
         default:
             validateCommand();
         }
