76280673b0de65dbcc074eda4eced7e60b90f1b6
Christal Huang <shihuang@berkeley.edu>
2020-07-20 11:49:27 -0700

    add test

--- lab13/BinaryTreeTest.java
+++ lab13/BinaryTreeTest.java
@@ -20,4 +20,28 @@ public class BinaryTreeTest {
         assertEquals("x in preorder\nC A B E D \nx in inorder\nA B C D E \n\n".trim(),
                      outContent.toString().trim());
     }
+
+    @Test
+    public void BSTAddTest() {
+        BinarySearchTree<String> x = new BinarySearchTree<String>();
+        x.add("C");
+        x.add("A");
+        x.add("E");
+        x.add("B");
+        x.add("D");
+        assertTrue(x.contains("D"));
+        assertEquals(false, x.contains("L"));
+    }
+
+    @Test
+    public void BSTAddTest1() {
+        BinarySearchTree<Integer> x = new BinarySearchTree<Integer>();
+        x.add(1);
+        x.add(5);
+        x.add(9);
+        x.add(11);
+        x.add(13);
+        assertTrue(x.contains(5));
+        assertEquals(false, x.contains(50));
+    }
 }
