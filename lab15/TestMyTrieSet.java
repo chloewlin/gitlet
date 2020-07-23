ae0c5b78b50ac702d5b654201ab48dc373a1e0d1
Chloe Lin <chloewjlin@gmail.com>
2020-07-23 12:33:50 -0700

    Complete MyTrieSet and pass all local tests

--- lab15/TestMyTrieSet.java
+++ lab15/TestMyTrieSet.java
@@ -10,47 +10,47 @@ public class TestMyTrieSet {
     // assumes add/contains work
     @Test
     public void sanityClearTest() {
-        // MyTrieSet t = new MyTrieSet();
-        // for (int i = 0; i < 455; i++) {
-        //     t.add("hi" + i);
-        //     //make sure put is working via contains
-        //     assertTrue(t.contains("hi" + i));
-        // }
-        // t.clear();
-        // for (int i = 0; i < 455; i++) {
-        //     assertFalse(t.contains("hi" + i));
-        // }
+         MyTrieSet t = new MyTrieSet();
+         for (int i = 0; i < 455; i++) {
+             t.add("hi" + i);
+             //make sure put is working via contains
+             assertTrue(t.contains("hi" + i));
+         }
+         t.clear();
+         for (int i = 0; i < 455; i++) {
+             assertFalse(t.contains("hi" + i));
+         }
     }
 
     // assumes add works
     @Test
     public void sanityContainsTest() {
-        // MyTrieSet t = new MyTrieSet();
-        // assertFalse(t.contains("waterYouDoingHere"));
-        // t.add("waterYouDoingHere");
-        // assertTrue(t.contains("waterYouDoingHere"));
+         MyTrieSet t = new MyTrieSet();
+         assertFalse(t.contains("waterYouDoingHere"));
+         t.add("waterYouDoingHere");
+         assertTrue(t.contains("waterYouDoingHere"));
     }
 
     // assumes add works
     @Test
     public void sanityPrefixTest() {
-        // String[] saStrings = new String[]{"same", "sam", "sad", "sap"};
-        // String[] otherStrings = new String[]{"a", "awls", "hello"};
+         String[] saStrings = new String[]{"same", "sam", "sad", "sap"};
+         String[] otherStrings = new String[]{"a", "awls", "hello"};
 
-        // MyTrieSet t = new MyTrieSet();
-        // for (String s: saStrings) {
-        //     t.add(s);
-        // }
-        // for (String s: otherStrings) {
-        //     t.add(s);
-        // }
+         MyTrieSet t = new MyTrieSet();
+         for (String s: saStrings) {
+             t.add(s);
+         }
+         for (String s: otherStrings) {
+             t.add(s);
+         }
 
-        // List<String> keys = t.keysWithPrefix("sa");
-        // for (String s: saStrings) {
-        //     assertTrue(keys.contains(s));
-        // }
-        // for (String s: otherStrings) {
-        //     assertFalse(keys.contains(s));
-        // }
+         List<String> keys = t.keysWithPrefix("sa");
+         for (String s: saStrings) {
+             assertTrue(keys.contains(s));
+         }
+         for (String s: otherStrings) {
+             assertFalse(keys.contains(s));
+         }
     }
 }
