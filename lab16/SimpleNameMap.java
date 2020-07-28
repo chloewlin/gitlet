85d283bbb09a45b6678ac2a83fb74c5b0a9935a3
Christal Huang <shihuang@berkeley.edu>
2020-07-27 18:54:12 -0700

    try to implement resize method

--- lab16/SimpleNameMap.java
+++ lab16/SimpleNameMap.java
@@ -6,16 +6,20 @@ public class SimpleNameMap {
 
     /* TODO: Instance variables here */
     private List<Entry>[] map = new List[26];
+    private List<Entry>[] resizeMap = new List[map.length * 2];
     private Integer count;
 
-//    public Integer hash(String key) {
+//    public int hash(String key) {
 //        return key.charAt(0) - 'A';
 //    }
 
     public SimpleNameMap() {
         this.count = 0;
         for (int i = 0; i < this.map.length; i++){
-            this.map[i] = new LinkedList<Entry>();
+            this.map[i] = new LinkedList<>();
+        }
+        for (int i = 0; i < this.resizeMap.length; i++){
+            this.resizeMap[i] = new LinkedList<>();
         }
     }
 
@@ -71,6 +75,13 @@ public class SimpleNameMap {
         return null;
     }
 
+    public void resize() {
+        for (int i = 0; i < this.map.length; i++){
+            this.resizeMap[i] = this.map[i];
+        }
+        this.map = this.resizeMap;
+    }
+
     /* Puts a (KEY, VALUE) pair into this map. If the KEY already exists in the
        SimpleNameMap, replace the current corresponding value with VALUE. */
     public void put(String key, String value) {
@@ -81,22 +92,20 @@ public class SimpleNameMap {
         boolean found = false;
 
         while (iter.hasNext()) {
-
             Entry entry = iter.next();
-
-            if (entry.equals(key)) {
-                found = true;
-//                entry.value = value;
+            if (count / this.map.length < this.map.length) {
+                if (entry.equals(key)) {
+                    found = true;
+                    entry.value = value;
+                }
             }
-            value = entry.value;
         }
-
         if (!found) {
             Entry newEntry = new Entry(key, value);
             linkedList.add(newEntry);
         }
-
         this.count = this.count + 1;
+
     }
 
     /* Removes a single entry, KEY, from this table and return the VALUE if
